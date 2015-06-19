e6ad89f39c9a531a9a1cffec2f9022874a0a3832
Alexandre Costa <alexandremcost@gmail.com>
2015-06-19 00:19:23 -0300

    skip cart

---
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -0,0 +1,12 @@
+Spree::OrdersController.class_eval do
+  before_action :skip_cart, only: :edit
+
+  private
+  def skip_cart
+    @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+    if !spree_current_user && @order.checkout_allowed? && @order.products.not_plan.empty?
+      @order.next
+      redirect_to checkout_state_path(@order.checkout_steps.first)
+    end
+  end
+end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Product.class_eval do
   add_search_scope(:not_plan) { where(plan: false) }
+  add_search_scope(:plan) { where(plan: true) }
 end
