cdc88384d9db91fafb0ff4301537469e7df8ee82
Alexandre Costa <alexandremcost@gmail.com>
2015-09-02 11:47:46 -0300

    search messages

--- app/controllers/spree/search_controller.rb
+++ app/controllers/spree/search_controller.rb
@@ -2,13 +2,22 @@ class Spree::SearchController < Spree::StoreController
   include Spree::Core::ControllerHelpers::User
 
   def result
-    @presenters = Spree::Search.new(search_params).presenters
-    redirect_to user_page_path(@presenters.first.page) if @presenters.size == 1
+    @presenters = search.presenters
+    if @presenters.size == 1
+      redirect_to user_page_path(@presenters.first.page),
+        flash: {notice: Spree.t('search.one_result')}
+    elsif search.message.present?
+      flash.now[:notice] = search.message
+    end
   end
 
   private
   def search_params
     params.permit(:query)
+  end
+
+  def search
+    @search ||= Spree::Search.new(search_params)
   end
 end
 
--- app/models/spree/search.rb
+++ app/models/spree/search.rb
@@ -1,6 +1,7 @@
 class Spree::Search
   include ActiveModel::Model
   attr_accessor :query
+  attr_reader :message
 
   def presenters
     raise NotImplementedError
